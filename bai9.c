88917ac9b4eb7420496283a002bd5ea1d3e9145f
Khoaibeti <khoai101205@gmail.com>
2025-04-17 10:48:46 +0700

    bai9.c

--- bai9.c
+++ bai9.c
@@ -1,66 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int thang,nam;
-    printf("Nhap lan luot gia tri cua thang va nam:"); 
-    scanf("%d %d",&thang,&nam);
-    if (nam % 4 ==0){
-        if (thang == 1){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 2){
-        printf( "Thang %d Nam %d co 29 ngay",thang,nam );
-        } else if (thang == 3){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 4){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 5){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 6){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 7){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 8){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 9){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 10){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 11){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 12){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else {
-            printf("So thang khong hop le vui long nhap lai");
-        }
-    } else {
-   if (thang == 1){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 2){
-        printf( "Thang %d Nam %d co 28 ngay",thang,nam );
-        } else if (thang == 3){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 4){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 5){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 6){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 7){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 8){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 9){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 10){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else if (thang == 11){
-        printf( "Thang %d Nam %d co 30 ngay",thang,nam );
-        } else if (thang == 12){
-        printf( "Thang %d Nam %d co 31 ngay",thang,nam );
-        } else {
-            printf("So thang khong hop le vui long nhap lai");
-        }
+    int thang, nam, ngay;
+
+    printf("Vui long nhap thang va nam: ");
+    scanf("%d%d", &thang&nam);
+        switch (thang) {
+        case 1: case 3: case 5: case 7:
+        case 8: case 10: case 12:
+            ngay = 31;
+            break;
+        case 4: case 6: case 9: case 11:
+            ngay = 30;
+            break;
+        case 2:
+            if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+                ngay = 29;
+            else
+                ngay = 28;
+            break;
+        default:
+            printf("Thang khong hop le!\n");
+            return 1;
     }
-    
+
+    printf("Thang %d nam %d co %d ngay.\n", thang, nam, ngay);
     return 0;
 }
