51549e7b903fd6ffef145a0bad95914f3423bcb0
Khoaibeti <khoai101205@gmail.com>
2025-04-17 11:30:11 +0700

    Bai14_for_do_while.c

--- Bai14_for_do_while.c
+++ Bai14_for_do_while.c
@@ -3,12 +3,10 @@
 int main() {
     double gdp2014, rate, gdp;
     int year = 2014;
-
     printf("Nhap GDP nam 2014 (ty USD): ");
     scanf("%lf", &gdp2014);
     printf("Nhap toc do tang truong hang nam (%%): ");
     scanf("%lf", &rate);
-
     printf("\n%-10s%-10s\n", "Nam", "GDP");
     gdp = gdp2014;
     while (gdp <= 2 * gdp2014) {
@@ -16,6 +14,5 @@ int main() {
         gdp += gdp * rate / 100.0;
         year++;
     }
-
     return 0;
 }
